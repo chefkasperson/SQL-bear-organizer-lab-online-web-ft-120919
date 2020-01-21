09ddf5ddfaf99223433de1da9efcf67dfb0ab964
chefkasperson <chefkasperson@gmail.com>
2020-01-20 22:49:29 -0800

    Done.

---
+++ lib/insert.sql
@@ -0,0 +1,15 @@
+INSERT INTO bears VALUES (1, 'Mr. Chocolate', 14, 'male', 'black', 'lazy', 1);
+
+INSERT INTO bears VALUES (2, 'Rowdy', 4, 'male', 'brown', 'aggressive', 1);
+
+INSERT INTO bears VALUES (3, 'Tabitha', 33, 'female', 'black and tan', 'crazy', 0);
+
+INSERT INTO bears VALUES (4, 'Sergeant Brown', 33, 'male', 'brown', 'bossy', 1);
+
+INSERT INTO bears VALUES (5, 'Melissa', 2, 'female', 'black', 'playful', 1);
+
+INSERT INTO bears VALUES (6, 'Grinch', 99, 'male', 'green', 'grouchy', 0);
+
+INSERT INTO bears VALUES (7, 'Wendy', 64, 'female', 'gray', 'thoughtful', 1);
+
+INSERT INTO bears VALUES (8, NULL, 204, 'male', 'white', 'dangerous', 1);
